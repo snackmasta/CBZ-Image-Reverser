f7c6144dab19c1c01a44e7ac2c812437b0732a30
currone <94299594+snackmasta@users.noreply.github.com>
2024-01-11 13:20:36 +0700

    Swap 2 filename in a dir

---
+++ import os.py
@@ -0,0 +1,17 @@
+import os
+
+def swap_file_names(directory):
+    files = os.listdir(directory)
+    for i in range(0, len(files), 2):
+        if (i+1) < len(files):
+            file1 = os.path.join(directory, files[i])
+            file2 = os.path.join(directory, files[i+1])
+            temp = os.path.join(directory, 'temp')
+            try:
+                os.rename(file1, temp)
+                os.rename(file2, file1)
+                os.rename(temp, file2)
+            except Exception as e:
+                print(f"Error swapping files {files[i]} and {files[i+1]}: {e}")
+
+swap_file_names(r"E:\Python\tes")
