cb6c633df7fa0cea22419826be4f3a843b5d98a5
currone <94299594+snackmasta@users.noreply.github.com>
2024-01-11 13:26:58 +0700

    swap pairing

--- import os.py
+++ import os.py
@@ -13,5 +13,15 @@ def swap_file_names(directory):
                 os.rename(temp, file2)
             except Exception as e:
                 print(f"Error swapping files {files[i]} and {files[i+1]}: {e}")
+    if len(files) % 2 != 0:
+        file1 = os.path.join(directory, files[0])
+        file2 = os.path.join(directory, files[-1])
+        temp = os.path.join(directory, 'temp')
+        try:
+            os.rename(file1, temp)
+            os.rename(file2, file1)
+            os.rename(temp, file2)
+        except Exception as e:
+            print(f"Error swapping files {files[0]} and {files[-1]}: {e}")
 
 swap_file_names(r"E:\Python\tes")
